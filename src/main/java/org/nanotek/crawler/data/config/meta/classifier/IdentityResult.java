ab6a6e092ed529ac6f34726af2a78b4f1cf62581
José Carlos Canova <jccanova.1967@gmail.com>
2022-11-06 01:46:37 -0300

    sketching classifier

--- src/main/java/org/nanotek/crawler/data/config/meta/classifier/IdentityResult.java
+++ src/main/java/org/nanotek/crawler/data/config/meta/classifier/IdentityResult.java
@@ -2,4 +2,29 @@ package org.nanotek.crawler.data.config.meta.classifier;
 
 public class IdentityResult {
 
+	public IdentityResult(IdentityType type) {
+		super();
+		this.type = type;
+	}
+
+	public IdentityResult() {
+		super();
+	}
+
+	private IdentityType type;
+	
+	public IdentityType getType() {
+		return type;
+	}
+
+	public void setType(IdentityType type) {
+		this.type = type;
+	}
+
+	public static enum IdentityType{
+		Single,
+		Composite
+	}
+	
 }
+
--- src/main/java/org/nanotek/crawler/data/config/meta/classifier/PrimaryKeyClassifier.java
+++ src/main/java/org/nanotek/crawler/data/config/meta/classifier/PrimaryKeyClassifier.java
@@ -1,12 +1,26 @@
 package org.nanotek.crawler.data.config.meta.classifier;
 
+import java.util.Optional;
+
+import org.nanotek.crawler.data.config.meta.classifier.IdentityResult.IdentityType;
+
 import schemacrawler.schema.PrimaryKey;
 
 public class PrimaryKeyClassifier implements Classifier<PrimaryKey , IdentityResult>{
 
 	@Override
-	public IdentityResult classify(PrimaryKey data) {
-		// TODO Auto-generated method stub
+	public IdentityResult classify(PrimaryKey pk) {
+		return Optional
+					.ofNullable(pk)
+					.map(k -> getIdentityType(k));
+					.map(t -> new IdentityResult(t , pk));
+	}
+
+	private IdentityResult.IdentityType getIdentityType(PrimaryKey k) {
+		if (k.getConstrainedColumns().size() == 1 )
+			return IdentityType.Single;
+		else (k.getConstrainedColumns().size() > 1) 
+			return IdentityType.Composite;
 		return null;
 	}
 
