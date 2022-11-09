f81bb2ec01ec923df1dd82c34f4e5079a7fff04e
José Carlos Canova <jccanova.1967@gmail.com>
2022-11-09 13:26:32 -0300

    update identityresult

--- src/main/java/org/nanotek/crawler/data/config/meta/classifier/IdentityResult.java
+++ src/main/java/org/nanotek/crawler/data/config/meta/classifier/IdentityResult.java
@@ -4,37 +4,33 @@ import schemacrawler.schema.PrimaryKey;
 
 public class IdentityResult {
 
-	public IdentityResult(IdentityType type) {
-		super();
-		this.type = type;
-	}
-
-	public IdentityResult() {
-		super();
-	}
-
 	private IdentityType type;
 	
 	private PrimaryKey key ; 
 	
+	private Enum<?> subType;
+	
+	public IdentityResult() {
+		super();
+	}
+
+	public IdentityResult(IdentityType type) {
+		super();
+		this.type = type;
+	}
+	
+	public IdentityResult(IdentityType type, PrimaryKey key) {
+		super();
+		this.type = type;
+		this.key = key;
+	}
+
 	public IdentityType getType() {
 		return type;
 	}
 
 	public void setType(IdentityType type) {
 		this.type = type;
-	}
-
-	public static enum IdentityType{
-		Single,
-		Composite,
-		Null
-	}
-
-	public IdentityResult(IdentityType type, PrimaryKey key) {
-		super();
-		this.type = type;
-		this.key = key;
 	}
 
 	public PrimaryKey getKey() {
@@ -45,5 +41,32 @@ public class IdentityResult {
 		this.key = key;
 	}
 	
+	
+	public static enum IdentityType{
+		Single,
+		Composite,
+		Null
+	}
+	
+	public static enum SingleIdentityType {
+		Identity , 
+		Sequence , 
+		Simple , 
+		Null
+	}
+	
+	public static enum CompositeIdentityType {
+		IdClass , 
+		Embembedable , 
+		Null
+	}
+
+	public Enum<?> getSubType() {
+		return subType;
+	}
+
+	public void setSubType(Enum<?> subType) {
+		this.subType = subType;
+	}
 }
 
